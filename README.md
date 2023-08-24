931332fad1c72ba7cc2ce8c57b7ad21355460c88
Daniel Merritt <daniel.merritt@uk.clara.net>
2023-08-24 17:37:52 +0100

    Adding bidirectional file transfer

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Features Integrity checking and SSL support. No External Dependencies required.
 Usage: 
 ```
 python3 server.py -p 80 (--ssl --certfile --keyfile)
-python3 client.py -p 80 <filename> <target> (--ssl)
+python3 client.py -p 80 <filename> <target> (--ssl --reverse)
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from os.path import exists
 import random
 import string
 import ssl
+import os
 
 
 def parse_args():
@@ -58,27 +59,62 @@ def md5sum_check(outfile, file_len, md5sum):
         print("Outfile integrity doesn't match!")
 
 
+def send_file(conn, filename):
+    file_len = os.stat(filename).st_size
+    md5sum = md5_file(filename, file_len)
+    metadata = {
+        "file_len": file_len,
+        "md5sum": md5sum,
+    }
+    json_metadata = json.dumps(metadata)
+    if len(json_metadata) <= 128:
+        padded_json_metadata = json_metadata.encode().ljust(128, b"\x00")
+    else:
+        print("Metadata too long!")
+        exit()
+
+    conn.sendall(padded_json_metadata)
+    print("Sent Metadata!")
+    remaining_len = file_len
+    with open(filename, "rb") as f:
+        while remaining_len > 0:
+            data = f.read(4096)
+            remaining_len -= 4096
+            conn.sendall(data)
+    print("Sent file!")
+
+
+def receive_file(conn, metadata):
+    file_len = metadata["file_len"]
+    md5sum = metadata["md5sum"]
+    outfile = metadata["filename"]
+    if not outfile:
+        outfile = metadata["filename"]
+    if exists(outfile):
+        outfile = "".join(random.choice(string.ascii_lowercase) for _ in range(10))
+        print(f"\nFile already Exists! Saving copy as {outfile}")
+
+    received_data = 0
+    with open(outfile, "wb") as f:
+        while received_data < file_len:
+            data = conn.recv(4096)
+            f.write(data)
+            received_data += len(data)
+    md5sum_check(outfile, file_len, md5sum)
+    print(f"\nReceived all data!")
+
+
 def accept_connection(sock, outfile):
     conn, addr = sock.accept()
     with conn:
         print(f"\nConnection from {addr[0]}")
         json_metadata = conn.recv(128).strip(b"\x00").decode()
         metadata = json.loads(json_metadata)
-        file_len = metadata["file_len"]
-        md5sum = metadata["md5sum"]
-        if not outfile:
-            outfile = metadata["filename"]
-        if exists(outfile):
-            outfile = "".join(random.choice(string.ascii_lowercase) for _ in range(10))
-            print(f"\nFile already Exists! Saving copy as {outfile}")
-
-        received_data = 0
-        with open(outfile, "wb") as f:
-            while received_data < file_len:
-                data = conn.recv(4096)
-                f.write(data)
-                received_data += len(data)
-    md5sum_check(outfile, file_len, md5sum)
+        if metadata["mode"] == "RECEIVE":
+            filename = metadata["filename"]
+            send_file(conn, filename)
+        else:
+            receive_file(conn, metadata)
 
 
 def main():
@@ -93,7 +129,6 @@ def main():
                 accept_connection(ssock, outfile)
         else:
             accept_connection(s, outfile)
-    print(f"\nReceived all data!")
 
 
 if __name__ == "__main__":
